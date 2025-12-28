f2498664beab3c27758fbe117d91848683d729b3
tofsir7 <khtofsir9191@gmail.com>
2025-12-28 11:13:37 +0600

    implement server error handling

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, ()=> {
+const server = app.listen(PORT);
+server.on('listening', ()=> {
     console.log(`Server is running on port ${PORT}`);
-})
+});
+
+server.on('error', (error) => {
+    if(error.code === 'EADDRINUSE') {
+        console.error(`ERROR: Port ${PORT} is already in use.\n`);
+        process.exit(1);
+    }
+    else if( error.code === 'EACCES') {
+        console.error(`ERROR: Permission denied to use this port\n`);
+        process.exit(1);
+    }
+    else {
+        console.error(`SERVER ERROR: ${error.message}\n`);
+        process.exit(1);
+    }
+});
